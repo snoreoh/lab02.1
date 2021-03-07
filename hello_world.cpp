603aadf03ce1ea6dd9073f9500a018849ea7ab25
snoreoh <nikita.hrapov@mail.ru>
2021-03-07 16:34:19 +0300

    The conflict has been finally fixed

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,9 +6,14 @@ main(int argc, char** argv)
 {
   std::string str;
   std::cout << "Your name:";
+<<<<<<< HEAD
   getline(std::cin, str);
   std::cout << "Hello world  from " << str
             << endl; // out'll be: Hello world <name>
+=======
+  getline(std::cin, str); 
+  std::cout<< "Hello world  from "<< str <<endl; // Вывод должен выглядеть: Hello world for <name>
+>>>>>>> changed comments
 
   return 1;
 }
