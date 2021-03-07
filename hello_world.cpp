b0f27a66314267544f57dbabaea65751f6c8a707
snoreoh <nikita.hrapov@mail.ru>
2021-03-07 12:04:07 +0300

    some more new updates to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,8 +5,8 @@ int main(int argc, char** argv)
 {
   std::string str;
   std::cout << "Your name:";
-  getline(std::cin, str);
-  std::cout<< "Hello world  from "<< str <<endl;
+  getline(std::cin, str); 
+  std::cout<< "Hello world  from "<< str <<endl; // out'll be: Hello world <name>
 
   return 1;
 }
