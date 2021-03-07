b4a26431bd683326e26d7e0b6fd02ab427b9c424
snoreoh <nikita.hrapov@mail.ru>
2021-03-07 17:02:43 +0300

    adad

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,8 +7,7 @@ main(int argc, char** argv)
   std::string str;
   std::cout << "Your name:";
   getline(std::cin, str);
-  std::cout << "Hello world  from " << str
-            << endl; // out'll be: Hello world <name>
+  std::cout << "Hello world  from " << str < endl; // out'll be: Hello world <name>
 
   return 1;
 }
