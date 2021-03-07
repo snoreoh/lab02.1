0ac57b943efe7f51b8c49b8c146b7ee809bf8e6b
snoreoh <nikita.hrapov@mail.ru>
2021-03-07 16:57:14 +0300

    Try again

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,35 +1,12 @@
 #include <iostream>
 #include <string>
 
-int
-main(int argc, char** argv)
+int main(int argc, char** argv)
 {
   std::string str;
   std::cout << "Your name:";
-<<<<<<< HEAD
-<<<<<<< HEAD
-  getline(std::cin, str);
-  std::cout << "Hello world  from " << str
-            << endl; // out'll be: Hello world <name>
-=======
   getline(std::cin, str); 
   std::cout<< "Hello world  from "<< str <<endl; // Вывод должен выглядеть: Hello world for <name>
->>>>>>> changed comments
-=======
-<<<<<<< ours
-  getline(std::cin, str); 
-  std::cout<< "Hello world  from "<< str <<endl; // Вывод должен выглядеть: Hello world for <name>
-=======
-  getline(std::cin, str);
-  std::cout << "Hello world  from " << str
-            << endl; // out'll be: Hello world <name>
->>>>>>> theirs
-=======
-  getline(std::cin, str);
-  std::cout << "Hello world  from " << str
-            << endl; // out'll be: Hello world <name>
->>>>>>> patch2
->>>>>>> 3903f826fca08b198218c218f962769cf465d2cc
 
   return 1;
 }
