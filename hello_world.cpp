7a6ecdbfd323c31e1a92157df747b19f242346d2
snoreoh <nikita.hrapov@mail.ru>
2021-03-07 11:02:32 +0300

    hello_world.cpp has been changed

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
   string str;
   cout << "Your name:";
   getline(cin, str);
-  cout<< "Hello world "<< str <<endl;
+  cout<< "Hello world  from "<< str <<endl;
 
   return 1;
 }
