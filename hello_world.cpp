5ed2662ef2905e55321a9a6b6feb1faa61bf3efd
snoreoh <nikita.hrapov@mail.ru>
2021-03-07 16:16:37 +0300

    Conflict has been fixed

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
   std::string str;
   std::cout << "Your name:";
+<<<<<<< ours
   getline(std::cin, str); 
   std::cout<< "Hello world  from "<< str <<endl; // Вывод должен выглядеть: Hello world for <name>
+=======
+  getline(std::cin, str);
+  std::cout << "Hello world  from " << str
+            << endl; // out'll be: Hello world <name>
+>>>>>>> theirs
 
   return 1;
 }
